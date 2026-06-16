9d19e0c856e614514c2f30b5320e86ebfb32e37e
contaimportante678-netizen <contaimportante678@gmail.com>
2026-06-16 11:08:25 -0300

    Add files via upload

---
+++ atividades.py
@@ -0,0 +1,25 @@
+maca= int(input("digite a quantidade"))
+
+
+if  maca< 12:
+  preco= float("1.30")
+
+else:
+ preco= float("1.00")
+
+total= preco * maca
+print( total)
+
+
+#atividade 2
+
+
+
+
+
+
+
+
+
+
+
